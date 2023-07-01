30202c5f553e7dec7f6d7da002f14f70030e56e7
korisu07 <y.kinokino.0712@gmail.com>
2023-07-02 00:45:56 +0900

    [settings] 設定・モジュール追加

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const isProduction = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  env: {
+    isProduction: isProduction,
+  },
+};
+
+module.exports = nextConfig;
