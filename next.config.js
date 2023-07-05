b1a5d01713b5312fece5c124cb4608edcb5b2eb2
korisu07 <y.kinokino07@gmail.com>
2023-07-06 01:29:00 +0900

    [settings] ホットリロード有効化。型チェック設定を修正。

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
-/** @type {import('next').NextConfig} */
-
 const isProduction = process.env.NODE_ENV === 'production';
 
+// @ts-check
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
     isProduction: isProduction,
   },
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, context) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300
+    }
+    return config
+  }
 };
 
 module.exports = nextConfig;
