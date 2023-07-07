23d427b6dcb3a2237fb44f50cbe4b49dd2882cad
korisu07 <y.kinokino07@gmail.com>
2023-07-08 04:16:44 +0900

    [settings] 読み込み設定とSASSに関する設定を変更／スペル設定追加

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const isProduction = process.env.NODE_ENV === 'production';
 
 // @ts-check
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   env: {
     isProduction: isProduction,
   },
@@ -10,11 +11,12 @@ const nextConfig = {
   swcMinify: true,
   webpack: (config, context) => {
     config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300
-    }
-    return config
-  }
+      poll: 4000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 };
-
-module.exports = nextConfig;
